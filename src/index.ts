a2b860ce87c548417679c87034a2f7250022c156
arm4n1 <armanraygeki18@gmail.com>
2025-01-23 10:00:24 +0800

    add register login and validation model and connect to database monggo db

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
 import express  from "express";
 import router from "./routes/api"
+import bodyParser from "body-parser";
+import db from "./utils/database";
 
-const app = express();
 
-const PORT = 3000;
+async function init() {
+    try {
 
-app.use('/api', router);
+        const result = await db();
 
-app.listen(PORT, () => {
-    console.log(`server is running on http://localhost:${PORT}`);
-})
+        console.log("database status:", result);
+
+        const app = express();
+
+        const PORT = 3000;
+
+        app.use(bodyParser.json());
+
+        app.use('/api', router);
+
+        app.listen(PORT, () => {
+            console.log(`server is running on http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+init();
+
